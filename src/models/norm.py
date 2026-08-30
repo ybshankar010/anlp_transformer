10ef6e3305027eb2a6eb5e212b0abf04b737221b
Shankar Y Bhavani <bhavani.shankar@fourkites.com>
2026-08-30 17:17:57 +0530

    Updating normalization functions

--- src/models/norm.py
+++ src/models/norm.py
@@ -5,14 +5,14 @@ import torch.nn as nn
 class LayerNorm(nn.Module):
     def __init__(self, d_model, eps=1e-5):
         super().__init__()
-        self.gamma = nn.Parameter(torch.ones(d_model))
-        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.weight = nn.Parameter(torch.ones(d_model))
+        self.bias = nn.Parameter(torch.zeros(d_model))
         self.eps = eps
 
     def forward(self, x):
         mean = x.mean(dim=-1, keepdim=True)
         variance = x.var(dim=-1, keepdim=True, unbiased=False)
-        return self.gamma * (x - mean) / torch.sqrt(variance + self.eps) + self.beta
+        return self.weight * (x - mean) / torch.sqrt(variance + self.eps) + self.bias
 
 
 class RMSNorm(nn.Module):
